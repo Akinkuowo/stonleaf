f5839ccb3b05e0deb2ab60524587d13fbdc5f90a
Inioluwa Nicole <inioluwa@Inioluwas-MacBook-Pro.local>
2026-02-24 02:28:15 +0100

    updating app

---
+++ server.js
@@ -0,0 +1,41 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+const path = require('path');
+
+// Set production environment
+process.env.NODE_ENV = 'production';
+
+const dev = false;
+const hostname = 'localhost';
+const port = process.env.PORT || 3000;
+
+// Path to the standalone build
+const app = next({
+    dev,
+    hostname,
+    port,
+    dir: path.join(__dirname)
+});
+
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+    createServer(async (req, res) => {
+        try {
+            const parsedUrl = parse(req.url, true);
+            await handle(req, res, parsedUrl);
+        } catch (err) {
+            console.error('Error occurred handling', req.url, err);
+            res.statusCode = 500;
+            res.end('Internal Server Error');
+        }
+    })
+        .once('error', (err) => {
+            console.error(err);
+            process.exit(1);
+        })
+        .listen(port, () => {
+            console.log(`> Ready on http://${hostname}:${port}`);
+        });
+});
